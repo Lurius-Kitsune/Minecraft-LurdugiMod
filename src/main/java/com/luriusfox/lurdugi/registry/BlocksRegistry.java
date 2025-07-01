5379f50ad7385a6e41d28e1bc06a972034e3eefc
Lurius-Kitsune <lucasfox@outlook.fr>
2025-07-01 15:37:09 +0200

    Added block

--- src/main/java/com/luriusfox/lurdugi/registry/BlocksRegistry.java
+++ src/main/java/com/luriusfox/lurdugi/registry/BlocksRegistry.java
@@ -16,7 +16,7 @@ public final class BlocksRegistry {
     private static RegistryObject<Block> RegisterBlocksAndItem(String _name, Supplier<? extends Block> _supplierBlock,  BlockItem.Properties _supplierItem) {
 
         RegistryObject<Block> _block = RegisterBlocks(_name, _supplierBlock);
-        ItemRegistry.ITEMS.register(_name, () -> new BlockItem(_block.get(), _supplierItem));
+        ItemRegistry.ITEMS.register(_name, () -> new BlockItem(_block.get(), _supplierItem.setId(ItemRegistry.ITEMS.key(_name))));
         return _block;
     }
 
@@ -29,7 +29,7 @@ public final class BlocksRegistry {
 
     //public static final RegistryObject<Block> TEST_BLOCK = BLOCKS.register("test_block", () -> new Block(Block.Properties.of().mapColor(MapColor.STONE).setId(BLOCKS.key("test_block"))));
 
-    public static final RegistryObject<Block> TEST_BLOCK = RegisterBlocksAndItem("test_block", () -> new TestBlock(), new BlockItem.Properties());
+    public static final RegistryObject<Block> TEST_BLOCK = RegisterBlocksAndItem(TestBlock.id,  () -> new TestBlock(), new BlockItem.Properties());
     
 
 }
