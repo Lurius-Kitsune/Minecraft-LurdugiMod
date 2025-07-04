f33af5b5616e111d2b5f35070610a9b4bf50a22c
Lurius-Kitsune <lucasfox@outlook.fr>
2025-07-04 17:20:58 +0200

    - Added craft test item

--- src/main/java/com/luriusfox/lurdugi/LurDuGiMod.java
+++ src/main/java/com/luriusfox/lurdugi/LurDuGiMod.java
@@ -2,22 +2,33 @@ package com.luriusfox.lurdugi;
 
 import java.util.List;
 import java.util.Vector;
-import java.util.logging.Logger;
 
 import com.luriusfox.lurdugi.registry.BlocksRegistry;
 import com.luriusfox.lurdugi.registry.ItemRegistry;
 import com.luriusfox.lurdugi.registry.TabsRegistry;
+import com.mojang.logging.LogUtils;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import org.slf4j.Logger;
 
 @Mod(LurDuGiMod.MODID)
 public final class LurDuGiMod {
     public static final String MODID = "lurdugi";
 
-    public static Logger LOGGER = Logger.getLogger(MODID);
+    public static Logger LOGGER = LogUtils.getLogger();
 
     private static Vector<DeferredRegister<?>> deferredRegisters = new Vector<DeferredRegister<?>>(
         List.of(ItemRegistry.ITEMS,
@@ -27,18 +38,63 @@ public final class LurDuGiMod {
 
     public LurDuGiMod(FMLJavaModLoadingContext _context) {
         LOGGER.info("LurDuGiMod is loading...");
-        RegisterRegistry(_context);
+        BusGroup _eventBus = _context.getModBusGroup();
 
-        //RegisterRegistry(_context);
+        // Register the commonSetup method for modloading
+        FMLCommonSetupEvent.getBus(_eventBus).addListener(this::commonSetup);
+        
+
+        RegisterRegistry(_eventBus);
+
+        // Register the item to a creative tab
+        BuildCreativeModeTabContentsEvent.getBus(_eventBus).addListener(LurDuGiMod::addCreative);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        _context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
         LOGGER.info("LurDuGiMod has loaded successfully.");
     }
 
 
-    private void RegisterRegistry(FMLJavaModLoadingContext _context)
+    private void RegisterRegistry(BusGroup _eventBus)
     {
-        BusGroup _eventBus = _context.getModBusGroup();
         for (DeferredRegister<?> _deferredRegister : deferredRegisters) {
             _deferredRegister.register(_eventBus);
         }
     }
+
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.logDirtBlock)
+            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+
+        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
+
+        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+    }
+
+    // Add custom item to default creative tab
+    private static void addCreative(BuildCreativeModeTabContentsEvent event) {
+        // if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
+        //     event.accept(EXAMPLE_BLOCK_ITEM);
+    }
+    
+    // list of all event
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        // This class is empty, but it is used to register client-side events
+        // You can add methods here to handle client-side events if needed
+
+         @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+
 }
--- src/main/java/com/luriusfox/lurdugi/registry/ItemRegistry.java
+++ src/main/java/com/luriusfox/lurdugi/registry/ItemRegistry.java
@@ -1,12 +1,17 @@
 package com.luriusfox.lurdugi.registry;
 
+import java.util.Set;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import com.luriusfox.lurdugi.LurDuGiMod;
+import com.luriusfox.lurdugi.items.LuriusItem;
+import com.luriusfox.lurdugi.items.SionixLockItem;
 import com.luriusfox.lurdugi.items.TestItem;
 import com.luriusfox.lurdugi.registry.TabsRegistry.TabType;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -17,6 +22,10 @@ public final class ItemRegistry {
 
     public static final RegistryObject<Item> TEST_ITEM = RegisterItems(TestItem.id, () -> new TestItem(), TabType.LURDUGI);
 
+    public static final RegistryObject<Item> SIONIX_LOCK_ITEM = RegisterItems(SionixLockItem.id, () -> new SionixLockItem(), TabType.LURDUGI);
+    public static final RegistryObject<Item> LURIUS_ITEM = RegisterItems(LuriusItem.id, () -> new LuriusItem(), TabType.LURDUGI);
+    public static final RegistryObject<Item> SIONIX_ITEM = RegisterItems("sionix",  () -> new Item(new Properties().setId(ITEMS.key("sionix"))));
+
 
     public static RegistryObject<Item> RegisterItems(String _name, Supplier<? extends Item> _item) {
         return ITEMS.register(_name, _item);
@@ -26,5 +35,11 @@ public final class ItemRegistry {
         RegistryObject<Item> _itemObject = RegisterItems(_name, _item);
         TabsRegistry.addToTab(_tabType, () -> _itemObject.get());
         return _itemObject;
+    }
+
+    public static Set<Item> getItems() {
+        return ITEMS.getEntries().stream()
+                .map(RegistryObject::get)
+                .collect(Collectors.toSet());
     }
 }
